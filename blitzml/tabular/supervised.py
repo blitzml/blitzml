3296f34168df9dba1ad15f4e7031e3b928aaf2da
Ahmed said <ahmedsaid2000321@gmail.com>
2023-08-22 04:43:53 +0300

    add supervised super class

---
+++ blitzml/tabular/supervised.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+
+class supervised():
+    
+    def get_custom_algorithm(self):
+        pass
+    
+    def drop_null_data(self):
+        pass
+    
+    def detect_target_data(self):
+        pass
+    
+    def concat_dataframes(self):
+        pass
+
+    def drop_null_columns(self):
+        pass
+    
+    def classify_columns(self):
+        pass
+
+    def classify_categorical_columns(self):
+        pass
+    
+    def fill_null_values(self):
+        pass
+    
+    def encode_categorical_columns(self):
+        pass
+    
+    def split_dataframes(self):
+        pass
+    
+    def split_train_validation(self):
+        pass
+    
+    def select_high_correlation(self):
+        pass
+    
+    def select_important_features(self):
+        pass
+
+    def used_cols(self):
+        pass
+    
+    def favorable_algorithm(self):
+        pass
+    
+    def train_the_model(self):
+        pass
